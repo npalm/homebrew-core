0b37cf192da00ac04305a9a27bacad5aa7168d98
Sam Ford <1584702+samford@users.noreply.github.com>
2024-11-03 13:45:59 -0500

    boost: update livecheck

--- Formula/b/boost.rb
+++ Formula/b/boost.rb
@@ -26,7 +26,7 @@ class Boost < Formula
     url "https://www.boost.org/users/download/"
     regex(/href=.*?boost[._-]v?(\d+(?:[._]\d+)+)\.t/i)
     strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| match.first.tr("_", ".") }
+      page.scan(regex).map { |match| match[0].tr("_", ".") }
     end
   end
 
